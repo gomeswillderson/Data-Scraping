a5712cc7fac3f64d0636c5a9a9dbe17fadcbb5b4
Will <gomeswillderson@gmail.com>
2023-04-14 19:35:34 -0300

    feat: fetch url
    Faz uma requisição HTTP GET para a URL fornecida

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,29 @@
-# Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+import time
+import requests
+
+
+def fetch(url: str) -> str:
+    """
+    Faz uma requisição HTTP GET para a URL fornecida,
+    respeitando um Rate Limit de 1 requisição por segundo.
+
+    Args:
+        url: str - A URL para fazer a requisição.
+
+    Returns:
+        str - O conteúdo HTML da resposta, caso a requisição
+        seja bem sucedida com Status Code 200: OK. None, caso contrário.
+    """
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        time.sleep(1)
+        response = requests.get(url, headers=headers, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
