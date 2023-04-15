9e52eef45dfe84d2ddc92920c83123e8af4c156e
Will <gomeswillderson@gmail.com>
2023-04-15 18:35:51 -0300

    feat: scrap news
    Extrai informações relevantes de uma página de notícia a partir do seu conteúdo HTML

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from typing import Optional
+from datetime import datetime
 
 
 def fetch(url: str) -> str:
@@ -63,9 +64,72 @@ def scrape_next_page_link(html_content: str) -> Optional[str]:
         return None
 
 
-# Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    """
+    Extrai informações relevantes de uma página de notícia
+    a partir do seu conteúdo HTML.
+
+    Args:
+        html_content (str): O conteúdo HTML da página de notícia.
+
+    Returns:
+        dict: Um dicionário contendo as informações extraídas da página,
+        incluindo o título,resumo, categoria, data de publicação, autor,
+        tempo de leitura e URL da notícia.
+    """
+    soup = BeautifulSoup(html_content, "html.parser")
+    news = {}
+    url = soup.find("link", {"rel": "canonical"})["href"]
+
+    # Check for news date and reading time in a specific class
+    meta_date = soup.find("li", {"class": ["meta-date", "post-modified-info"]})
+    if meta_date:
+        news_date = meta_date.text.split(" ")[0]
+        news["timestamp"] = datetime.strptime(news_date, "%d/%m/%Y").strftime(
+            "%d/%m/%Y"
+        )
+
+        reading_time = soup.find("li", {"class": "meta-reading-time"})
+        if reading_time:
+            reading_time = reading_time.text.split(" ")[0]
+            news["reading_time"] = int(reading_time)
+
+        news["category"] = soup.find("span", {"class": "label"}).text
+    else:
+        # Check for reading time in a specific meta tag
+        reading_time = soup.find(
+            "meta",
+            {
+                "name": lambda value: value
+                and str(value).startswith("twitter:data"),
+                "content": lambda value: value if "minutos" in value else "",
+            },
+        )
+        if reading_time:
+            reading_time = reading_time["content"].split(" ")[0]
+            news["reading_time"] = int(reading_time)
+
+        # Check for news date in a specific class
+        news_date = soup.find("div", {"class": "topic-page-title-meta"})
+        if news_date:
+            news_date = news_date.text.replace("Última atualização", "")
+            news["timestamp"] = datetime.strptime
+            (news_date, "%d/%B/%Y").strftime(
+                "%d/%m/%Y"
+            )
+
+        # Extract category from URL
+        news["category"] = url.split("/")[3].title()
+
+    # Extract remaining news information
+    news["url"] = url
+    news["title"] = soup.find("h1", {"class": "entry-title"}).text.strip()
+    news["writer"] = soup.find("a", {"class": "url fn n"}).text
+    news["summary"] = (
+        soup.find("div", {"class": "entry-content"}).find("p").text.strip()
+    )
+
+    return news
 
 
 # Requisito 5
