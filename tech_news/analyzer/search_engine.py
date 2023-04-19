036d1edd599937f35173d2d04072697706b155ec
Will <gomeswillderson@gmail.com>
2023-04-19 13:31:05 -0300

    feat: search by date
    Busca notícias no banco de dados a partir de uma data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
-from tech_news.database import db
+from tech_news.database import db, search_news
+from datetime import datetime
 
 
 def search_by_title(title: str) -> List[Tuple[str, str]]:
@@ -25,9 +26,29 @@ def search_by_title(title: str) -> List[Tuple[str, str]]:
     return result
 
 
-# Requisito 8
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date: str) -> list[tuple[str, str]]:
+    """
+    Busca notícias no banco de dados a partir de uma data.
+
+    Args:
+        date (str): Uma string representando uma data no formato ISO.
+
+    Returns:
+        Uma lista de tuplas contendo o título e a URL das notícias encontradas.
+
+    Raises:
+        ValueError: se a data fornecida estiver em formato inválido.
+
+    """
+    try:
+        date_formater = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    find = search_news({"timestamp": date_formater})
+    news_by_date = [(news["title"], news["url"]) for news in find]
+
+    return news_by_date
 
 
 # Requisito 9
