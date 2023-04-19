a3db5ddd0b84d72a9b4f401a5cf84c9e15f41a35
Will <gomeswillderson@gmail.com>
2023-04-19 13:20:36 -0300

    feat: search by title
    Busca notícias por título no banco de dados

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,28 @@
-# Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+from typing import List, Tuple
+from tech_news.database import db
+
+
+def search_by_title(title: str) -> List[Tuple[str, str]]:
+    """
+    Busca notícias por título no banco de dados.
+
+    Args:
+        title: string com o título da notícia a ser buscada.
+
+    Returns:
+        Uma lista de tuplas com as notícias encontradas na busca, contendo
+        o título e a URL da notícia. Exemplo:
+        [
+          ("Título1_aqui", "url1_aqui"),
+          ("Título2_aqui", "url2_aqui"),
+          ("Título3_aqui", "url3_aqui"),
+        ]
+
+        Caso nenhuma notícia seja encontrada, uma lista vazia é retornada.
+    """
+    news = db.news.find({"title": {"$regex": f".*{title}.*", "$options": "i"}})
+    result = [(n["title"], n["url"]) for n in news]
+    return result
 
 
 # Requisito 8
